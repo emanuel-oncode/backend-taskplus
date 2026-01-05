4a60b38fc790b8193e0080bfa6ef42f109b3e776
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-05 17:58:11 -0300

    createPool mysql

---
+++ src/Config/dbConexion.js
@@ -0,0 +1,9 @@
+import { createPool } from "mysql2";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from "./env.config.js";
+
+export const pool = createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+});
