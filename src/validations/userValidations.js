6537dc34b0f97a0b0383e50a13ed7d3f63479498
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-08 14:23:47 -0300

    userValidations

--- src/validations/userValidations.js
+++ src/validations/userValidations.js
@@ -1,40 +1,39 @@
 export function validateEmail(email) {
-  let objetMessage = { ok: true, message: "Everything is fine." };
+  if (!email) return { ok: false, message: "Email is required" };
 
-  if (!email) objetMessage = { ok: false, message: "Email is required" };
+  if (!email.includes("@")) return { ok: false, message: `Must include an @` };
 
-  if (!email.includes("@"))
-    objetMessage = { ok: false, message: `Must include an "@"` };
+  if (!email.includes(".")) return { ok: false, message: `Must include an .` };
 
-  if (!email.includes("."))
-    objetMessage = { ok: false, message: `Must include an "."` };
-
-  return objetMessage;
+  return { ok: true, message: "Everything is fine." };
 }
 
 export function validatePasssword(password) {
-  let objetMessage = { ok: true, message: "Everything is fine." };
-
-  if (!password) objetMessage = { ok: false, message: "Password is required" };
+  if (!password) return { ok: false, message: "Password is required" };
 
   if (password.lenght < 8) {
-    objetMessage = {
+    return {
       ok: false,
       message: `The password must be at least 8 digits long.`,
     };
   }
 
-  return objetMessage;
+  return { ok: true, message: "Everything is fine." };
 }
 
 export function validateName(name) {
-  let objetMessage = { ok: true, message: "Everything is fine." };
+  console.log(name.lenght);
+  if (!name)
+    return {
+      ok: false,
+      message: "Name is required",
+    };
 
-  if (!name.lenght > 3)
-    return (objetMessage = {
+  if (name.lenght < 3)
+    return {
       ok: false,
       message: "The name must have 3 or more characters",
-    });
+    };
 
-  return objetMessage;
+  return { ok: true, message: "Everything is fine." };
 }
