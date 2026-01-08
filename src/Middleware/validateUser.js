20785ac657f04929454f4180619ca3b00197f91f
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-08 00:16:51 -0300

    middleware validation

--- src/Middleware/validateUser.js
+++ src/Middleware/validateUser.js
@@ -1,4 +1,36 @@
+import {
+  validateEmail,
+  validateName,
+  validatePasssword,
+} from "../validations/userValidations.js";
+
+// TODO: Validar fecha!
 function validateUser(req, res, next) {
-  const { userName, userLastName, userBirthdate, userEmail, userPassword } =
-    req.body;
+  const { userName, userEmail, userPassword } = req.body;
+
+  const emailIsValid = validateEmail(userEmail);
+  if (emailIsValid.ok) {
+    return res.status(400).json({
+      success: false,
+      message: emailIsValid.message,
+    });
+  }
+
+  const nameIsValid = validateName(userName);
+  if (nameIsValid.ok) {
+    return res.status(400).json({
+      success: false,
+      message: nameIsValid.message,
+    });
+  }
+
+  const passwordIsValid = validatePasssword(userPassword);
+  if (passwordIsValid.ok) {
+    return res.status(400).json({
+      success: false,
+      message: passwordIsValid.message,
+    });
+  }
+
+  next();
 }
