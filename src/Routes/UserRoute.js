9277b4e71f1fefa8ef87cfb14fd0666002014b09
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-08 14:23:36 -0300

    UserRoute

--- src/Routes/UserRoute.js
+++ src/Routes/UserRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { UserController } from "../Controllers/UserController.js";
+import { validateUser } from "../Middleware/validateUser.js";
 
 export const userRoute = Router();
 
 userRoute.get("/", UserController.getUsers);
 userRoute.get("/userById", UserController.getUserById);
 
-userRoute.post("/", UserController.createUser);
+userRoute.post("/", validateUser, UserController.createUser);
