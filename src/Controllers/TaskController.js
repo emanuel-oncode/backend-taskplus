5f07589e0ee099e976d934e36f58143ede7d2f7c
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-15 16:33:37 -0300

    arreglando algunas webadas codigo de estado y session del usuario

--- src/Controllers/TaskController.js
+++ src/Controllers/TaskController.js
@@ -2,13 +2,14 @@ import { TaskModel } from "../Models/TaskModel.js";
 
 export class TaskController {
   static async getTaskUser(req, res) {
-    const user_id = req.session.user_id;
+    const user_id = req.user.id;
 
+    console.log("user id desde el controlador getTaskUser:", user_id);
     try {
       const result = await TaskModel.getTask(user_id);
 
       if (!result) {
-        return res.status(400).json({
+        return res.status(200).json({
           success: false,
           message: "No hay tareas creadas",
         });
@@ -45,6 +46,28 @@ export class TaskController {
         success: true,
         message: "Se crea lo tarea de forma exitosa",
         result,
+      });
+    } catch (error) {}
+  }
+
+  static async toggleCompleted(req, res) {
+    const user_id = req.user.id;
+    const { task_id } = req.body;
+
+    console.log("toggel userid: ", user_id);
+    try {
+      const result = await TaskModel.toggleTaskCompleted(task_id, user_id);
+
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: "Tarea no encontrada",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Estado cambiado exitosamente",
       });
     } catch (error) {}
   }
