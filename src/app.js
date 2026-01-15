a13c39745643257c36437539d0b47f1a3595a09b
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-15 16:32:52 -0300

    añadiendo rutas de las tareas y cors

--- src/app.js
+++ src/app.js
@@ -6,16 +6,18 @@ import cookieParser from "cookie-parser";
 
 export const app = express();
 
-app.use(express.json());
-
-app.use(cookieParser());
-
 app.use(
   cors({
     origin: "http://localhost:4321",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Set-Cookie"],
   })
 );
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/user", userRoute);
 app.use("/user/task", taskRoute);
