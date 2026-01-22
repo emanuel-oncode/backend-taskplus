8c1a29506190c0f404fb21f6a2f6e53034804443
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-22 14:08:11 -0300

    add rooutefinance

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userRoute } from "./Routes/UserRoute.js";
 import { taskRoute } from "./Routes/TaskRoute.js";
+import { financeRoute } from "./Routes/FinanceRoute.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -13,7 +14,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     exposedHeaders: ["Set-Cookie"],
-  })
+  }),
 );
 
 app.use(express.json());
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use("/user", userRoute);
 app.use("/user/task", taskRoute);
+app.use("/user/finance", financeRoute);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "The API is working" });
