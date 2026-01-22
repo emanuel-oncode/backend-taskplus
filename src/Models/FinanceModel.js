addb5dca95f2967fa50a4b5e96bd8b1052db664e
Esteban Emanuel <esteban.emanuel.flores@gmail.com>
2026-01-21 23:02:18 -0300

    finance model

--- src/Models/FinanceModel.js
+++ src/Models/FinanceModel.js
@@ -1,16 +1,52 @@
 import { pool } from "../Config/dbConexion.js";
 
+//? CREATE TABLE finance (
+//?     finance_id CHAR(32) PRIMARY KEY,
+//?     user_id CHAR(32) NOT NULL REFERENCES user(user_id),
+//!     finance_type VARCHAR(10) CHECK (finance_type IN ('income', 'exit')),
+//?     amount DECIMAL(15, 2) NOT NULL,
+//?     finance_description TEXT,
+//?     finance_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//? );
+
 export class FinanceModel {
-  static async postIngreso(amount, user_id) {
+  static async postFinance(
+    finance_id,
+    amount,
+    user_id,
+    finance_type,
+    finance_description,
+  ) {
     try {
       const [result] = await pool.execute(
         `
-          INSERT INTO finance(amount, user_id) VALUES (?,?)
+          INSERT INTO finance(finance_id, amount, user_id, finance_type, finance_description) VALUES (?,?,?,?,?)
         `,
-        [amount, user_id],
+        [finance_id, amount, user_id, finance_type, finance_description],
       );
 
       return result;
-    } catch (error) {}
+    } catch (error) {
+      console.error("Error al crear una finanza:", error);
+
+      throw error;
+    }
+  }
+
+  static async getFinance(user_id, finance_type) {
+    try {
+      const [result] = await pool.execute(
+        `
+          SELECT * FROM finance WHERE user_id = ? AND finance_type = ?
+        `,
+        [user_id, finance_type],
+      );
+
+      return result;
+    } catch (error) {
+      console.error("Error al obtener finanzas:", error);
+
+      throw error;
+    }
   }
 }
